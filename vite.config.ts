d3b6ba13254f739c7c6d1c09085780df7cc9f8a7
beeman <beeman@beeman.nl>
2024-08-30 00:34:14 +0100

    feat: add remix-express-dev-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { expressDevServer } from "remix-express-dev-server";
+
+installGlobals({ nativeFetch: true });
 
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
+    expressDevServer(),
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        unstable_singleFetch: true,
       },
     }),
     tsconfigPaths(),
